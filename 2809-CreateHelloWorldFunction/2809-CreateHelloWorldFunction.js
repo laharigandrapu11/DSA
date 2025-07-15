ca58e234332a5e0293fa502bff6545e5aaf80f1f
Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-07-15 16:46:39 -0400

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 7/15/2025, 4:46:39 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
