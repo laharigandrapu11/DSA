558f6238a3b89dd769374f9cc92e88d92a6376b2
Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-07-16 14:17:59 -0400

    Create 3201-FindtheMaximumLengthofValidSubsequenceI

---
+++ 3201-FindtheMaximumLengthofValidSubsequenceI/version/python/3201-FindtheMaximumLengthofValidSubsequenceI_20250716_141758.py
@@ -0,0 +1,15 @@
+# Last updated: 7/16/2025, 2:17:58 PM
+class Solution(object):
+    def maximumLength(self, nums):
+        c = nums[0] % 2
+        odd = even = both = 0
+        for num in nums:
+            if num % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+            if num % 2 == c:
+                both += 1
+                c = 1 - c  # Toggle the parity
+        return max(both, even, odd)
+        
