e9da96e1851262aef2d5bac98b0c744118ee77c7
Sri Sai Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-07-19 15:18:48 -0400

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.py
@@ -0,0 +1,10 @@
+# Last updated: 7/19/2025, 3:18:47 PM
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if i != j and arr[i] == 2 * arr[j]:
+                    return True
+                
+        return False
+        
