6d8ed52b9804aafbae01210f29eb40b8ea8200f1
Sri Sai Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-07-19 16:22:45 -0400

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/version/python/217-ContainsDuplicate_20250719_162245.py
@@ -0,0 +1,15 @@
+# Last updated: 7/19/2025, 4:22:45 PM
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        nh = {}
+        for n in nums:
+            if n in nh:
+                return True
+            else:
+                nh[n] = 1
+        return False
+        
+        
+
+        
