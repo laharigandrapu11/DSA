d066de0e444dd0fdc28c6f8ec49ef735c9319b7f
Sri Sai Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-07-25 17:11:38 -0400

    Create 344-ReverseString

---
+++ 344-ReverseString/version/python/344-ReverseString_20250725_171137.py
@@ -0,0 +1,9 @@
+# Last updated: 7/25/2025, 5:11:37 PM
+class Solution:
+    def countSegments(self, s: str) -> int:
+        if len(s)==0:
+            return 0
+        arr = s.split()
+     
+        return len(arr)
+        
