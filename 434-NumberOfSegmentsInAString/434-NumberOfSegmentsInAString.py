30926491a5e3f6ce202481ced785a434dc070ff9
Sri Sai Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-07-27 16:54:40 -0400

    Create 434-NumberOfSegmentsInAString

---
+++ 434-NumberOfSegmentsInAString/434-NumberOfSegmentsInAString.py
@@ -0,0 +1,9 @@
+# Last updated: 7/27/2025, 4:54:39 PM
+class Solution:
+    def countSegments(self, s: str) -> int:
+        if len(s)==0:
+            return 0
+        arr = s.split()
+     
+        return len(arr)
+        
