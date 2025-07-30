0f896d0301125d2d8fcd68be7c9e8095249fb6f0
Sri Sai Lahari Gandrapu <laharigandrapu11@gmail.com>
2025-07-30 15:10:49 -0400

    Create 1768-MergeStringsAlternately

---
+++ 1768-MergeStringsAlternately/version/python/1768-MergeStringsAlternately_20250730_151048.py
@@ -0,0 +1,13 @@
+# Last updated: 7/30/2025, 3:10:48 PM
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        comb = ""
+        max_len = max(len(word1), len(word2))
+        for i in range(max_len):
+            if i < len(word1):
+                comb += word1[i]
+            if i < len(word2):
+                comb += word2[i]
+        return comb
+
+        
